04bfcffd4f36d55bd592ae2eab046d01361c6a8a
proshehab <shehab.shakib@gmail.com>
2025-11-19 18:58:47 +0600

    new update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\FrontendController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [FrontendController::class, 'index'])->name('frontend.index');
